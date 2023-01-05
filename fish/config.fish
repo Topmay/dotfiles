418bf382a0772a14716d4e57a14d97add3d3e47f
arg <guptaarnav376@gmail.com>
2023-01-06 01:23:32 +0530

    add aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Commands to run in interactive sessions can go here
 neofetch
-set fish_greeting                                        #supress fish intro
+set fish_greeting                                                 #supress fish intro
+
+#Aliases
+alias ls='exa -al --color=always --group-directories-first'       #change ls to exa
+alias n='nvim'                                                    #neovim
 end
